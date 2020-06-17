a34fcb3581e8b50a69c138d2fb8808fc3aac1320
EC2 Default User <ec2-user@ip-172-24-10-110.ap-northeast-2.compute.internal>
2020-06-17 06:31:11 +0000

    test it in bash.

--- resources/ping-test.sh
+++ resources/ping-test.sh
@@ -8,27 +8,33 @@ local_ipv4=`curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254
 status=''
 
 function retrieveStatus() {
-  status=`aws dynamodb get-item --table-name '${target_ddb_table}' --key '{ "pk" : { "S": "CONTROL" } , "sk" : {"S" : "STATUS_KEY" } }' | jq '.Item.value.S'`
+  status=`aws dynamodb get-item --table-name ${target_ddb_table} --key '{ "pk" : { "S": "CONTROL" } , "sk" : {"S" : "STATUS_KEY" } }' | jq '.Item.value.S' | sed '1,$s/"//g'` 
 }
 
-function retrieveStatus() {
-
-}
+echo ${az_loc}
+echo ${local_ipv4}
+echo ${status}
 
 function putLocalIp() {
-  aws dynamodb put-time --tablename '${target_ddb_table}' --item '{ "pk": {"S" : "IP" }, "sk" : { "S" : "${local_ipv4}"}'
+  aws dynamodb put-item --table-name ${target_ddb_table} --item '{ "pk": {"S" : "IP" }, "sk" : { "S" : "'${local_ipv4}'" } }'
 }
+
+putLocalIp
 
 while(true) 
 do
-  retrieveStatus()
+  retrieveStatus
+  echo current status is ${status}
+  sleep 1
   case "${status}" in
   START)
-    
+    echo 'Start' 
   ;;
   STOP)
+    echo 'Stop'
   ;;
   TERMINATE)
+    echo 'termicate'
   ;;
   *)
   echo "There is no matched status in DDB table (${target_ddb_table})"
@@ -37,7 +43,4 @@ do
 done
 
 echo "all process is done."
-
-
-}
 
